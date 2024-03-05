c0dee0d6dd925ffe190f7d74a51e7657e72df49b
deepaksharmadk <dks847203@gmail.com>
2024-03-05 22:53:46 +0530

    Added user model

---
+++ ecomm/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    userid: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      minLength: 10,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+
+    userType: {
+      type: String,
+      default: "USER",
+      enum: ["USER", "ADMIN"],
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+const User = mongoose.model("User", userSchema);
